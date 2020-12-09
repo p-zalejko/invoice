a4be4e57c82c65f40a1506997f115213d32a712a
Pawel Zalejko <p.zalejko@gmail.com>
2020-12-09 23:14:38 +0100

    fixed a compilation error

--- web/src/main/kotlin/com/gmail/pzalejko/invoice/invoicerequest/infrastructure/InvoiceRequestDatabaseRepository.kt
+++ web/src/main/kotlin/com/gmail/pzalejko/invoice/invoicerequest/infrastructure/InvoiceRequestDatabaseRepository.kt
@@ -1,6 +1,16 @@
 package com.gmail.pzalejko.invoice.invoicerequest.infrastructure
 
+import com.gmail.pzalejko.invoice.invoicerequest.model.InvoiceRequest
 import com.gmail.pzalejko.invoice.invoicerequest.model.InvoiceRequestRepository
+import com.gmail.pzalejko.invoice.model.InvoiceNumber
 
 class InvoiceRequestDatabaseRepository: InvoiceRequestRepository {
+
+    override fun findByNumber(number: InvoiceNumber): InvoiceRequest? {
+        TODO("Not yet implemented")
+    }
+
+    override fun save(request: InvoiceRequest) {
+        TODO("Not yet implemented")
+    }
 }

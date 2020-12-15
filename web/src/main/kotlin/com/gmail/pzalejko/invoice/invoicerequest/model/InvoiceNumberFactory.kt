9ea579184afc79fff34da5ff2f96966386e3560b
Pawel Zalejko <p.zalejko@gmail.com>
2020-12-15 22:57:45 +0100

    fixed compilation error

--- web/src/main/kotlin/com/gmail/pzalejko/invoice/invoicerequest/model/InvoiceNumberFactory.kt
+++ web/src/main/kotlin/com/gmail/pzalejko/invoice/invoicerequest/model/InvoiceNumberFactory.kt
@@ -1,10 +1,11 @@
 package com.gmail.pzalejko.invoice.invoicerequest.model
 
 import com.gmail.pzalejko.invoice.model.InvoiceNumber
+import com.gmail.pzalejko.invoice.model.MonthBasedInvoiceNumber
 
 class InvoiceNumberFactory {
 
     fun getNextNumber(): InvoiceNumber {
-        return InvoiceNumber("")
+        return MonthBasedInvoiceNumber(1,1,2020)
     }
 }

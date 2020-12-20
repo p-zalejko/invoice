e866ba2e6ff51ac193e208703461e94470b2725d
Pawel Zalejko <p.zalejko@gmail.com>
2020-12-20 22:06:06 +0100

    initial setup of AWS DynamoDB

--- web/src/main/kotlin/com/gmail/pzalejko/invoice/invoicerequest/infrastructure/InvoiceRequestDatabaseRepository.kt
+++ web/src/main/kotlin/com/gmail/pzalejko/invoice/invoicerequest/infrastructure/InvoiceRequestDatabaseRepository.kt
@@ -3,10 +3,17 @@ package com.gmail.pzalejko.invoice.invoicerequest.infrastructure
 import com.gmail.pzalejko.invoice.invoicerequest.model.InvoiceRequest
 import com.gmail.pzalejko.invoice.invoicerequest.model.InvoiceRequestRepository
 import com.gmail.pzalejko.invoice.model.InvoiceNumber
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.inject.Default
+import javax.inject.Inject
 
 @ApplicationScoped
 class InvoiceRequestDatabaseRepository: InvoiceRequestRepository {
+
+    @Inject
+    @field: Default
+    lateinit var dynamoDB: DynamoDbClient
 
     override fun findByNumber(number: InvoiceNumber): InvoiceRequest? {
         TODO("Not yet implemented")

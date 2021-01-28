57f95bb590557d258489a9033b591883488ec717
Pawel Zalejko <p.zalejko@gmail.com>
2021-01-28 22:37:49 +0100

    fixed how currency is compared

--- web/src/main/kotlin/com/gmail/pzalejko/invoice/common/model/InvoiceItemPrice.kt
+++ web/src/main/kotlin/com/gmail/pzalejko/invoice/common/model/InvoiceItemPrice.kt
@@ -10,13 +10,12 @@ import java.util.*
 data class InvoiceItemPrice(val value: BigDecimal, val taxPercentage: BigDecimal, val currency: Currency) : ValueObject {
 
     companion object {
-        private val SUPPORTED_LOCALE = Locale("pl", "PL")
-        private val SUPPORTED_CURRENCY = Currency.getInstance(SUPPORTED_LOCALE)
+        private val SUPPORTED_CURRENCY = Currency.getInstance("PLN")
     }
 
     init {
         require(value > BigDecimal.ZERO) { "Price must be grater than zero" }
         require(taxPercentage >= BigDecimal.ZERO) { "Price must be grater or equals than zero" }
-        require(currency == SUPPORTED_CURRENCY) { "Only PLN (Poland) currency is supported." }
+        require(currency.currencyCode == SUPPORTED_CURRENCY.currencyCode) { "Only PLN (Poland) currency is supported." }
     }
 }

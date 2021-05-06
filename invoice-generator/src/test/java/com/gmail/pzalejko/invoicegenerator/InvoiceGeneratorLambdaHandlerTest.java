24edcb025403cd1bdafa9a7bccbee4e17b1956be
Pawel Zalejko <p.zalejko@gmail.com>
2021-05-06 23:09:23 +0200

    clean up

--- invoice-generator/src/test/java/com/gmail/pzalejko/invoicegenerator/InvoiceGeneratorLambdaHandlerTest.java
+++ invoice-generator/src/test/java/com/gmail/pzalejko/invoicegenerator/InvoiceGeneratorLambdaHandlerTest.java
@@ -4,39 +4,31 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.amazon.lambda.test.LambdaClient;
 import io.quarkus.test.junit.QuarkusTest;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @QuarkusTest
 public class InvoiceGeneratorLambdaHandlerTest {
 
-    @Test
-    public void testSimpleLambdaSuccess() throws Exception {
-        try (InputStream in = InvoiceGeneratorLambdaHandlerTest.class.getResourceAsStream("/payload.json");) {
-            String jsonString = readStream(in);
+    private JsonNode samplePayload;
 
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode node = mapper.readTree(jsonString);
-
-            InvoiceRequestOutput out = LambdaClient.invoke(InvoiceRequestOutput.class, node);
-            Assertions.assertNotNull(out);
+    @BeforeEach
+    void setUp() throws IOException {
+        try (var in = InvoiceGeneratorLambdaHandlerTest.class.getResourceAsStream("/payload.json")) {
+            var asString = IOUtils.toString(in, StandardCharsets.UTF_8);
+            var mapper = new ObjectMapper();
+            samplePayload = mapper.readTree(asString);
         }
-
-
     }
 
-    private static String readStream(InputStream in) throws IOException {
-        byte[] data = new byte[1024];
-        int r;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        while ((r = in.read(data)) > 0) {
-            out.write(data, 0, r);
-        }
-        return out.toString(StandardCharsets.UTF_8);
+    @Test
+    public void testSimpleLambdaSuccess() {
+        InvoiceRequestOutput out = LambdaClient.invoke(InvoiceRequestOutput.class, samplePayload);
+        Assertions.assertNotNull(out);
     }
 }

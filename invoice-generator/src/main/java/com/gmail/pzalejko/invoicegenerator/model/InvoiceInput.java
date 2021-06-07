9fe4b40a70b48d56dc440eb547be3110fdb9a279
Pawel Zalejko <p.zalejko@gmail.com>
2021-06-07 22:26:54 +0200

    quarkus upgrade, java upgrade

--- invoice-generator/src/main/java/com/gmail/pzalejko/invoicegenerator/model/InvoiceInput.java
+++ invoice-generator/src/main/java/com/gmail/pzalejko/invoicegenerator/model/InvoiceInput.java
@@ -1,14 +1,28 @@
 package com.gmail.pzalejko.invoicegenerator.model;
 
 import java.time.LocalDate;
+import java.util.Currency;
 import java.util.List;
 
 public record InvoiceInput(
         long accountId,
         String invNumber,
-        List<InvoiceItemInput> items,
+        List<Item> items,
         String clientDetails,
         LocalDate dueDate,
         LocalDate saleDate,
         LocalDate creationDate) {
+
+    public record Item(
+            String name,
+            long count,
+            String unit,
+            rice price) {
+    }
+
+    public record rice(
+            double pricePerOne,
+            double taxPerOne,
+            Currency currency) {
+    }
 }

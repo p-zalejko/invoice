3bb354be54ea7a135c67122bc2e121d4a916fa8a
Pawel Zalejko <p.zalejko@gmail.com>
2021-06-09 21:32:57 +0200

    clean up

--- invoice-generator/src/main/java/com/gmail/pzalejko/invoicegenerator/application/Main.java
+++ invoice-generator/src/main/java/com/gmail/pzalejko/invoicegenerator/application/Main.java
@@ -1,71 +1,67 @@
-package com.gmail.pzalejko.invoicegenerator.application;
-
-import com.openhtmltopdf.extend.FSSupplier;
-import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import org.jsoup.Jsoup;
-import org.jsoup.helper.W3CDom;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import java.awt.*;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws IOException, TemplateException {
-        Properties prop = new Properties();
-        try (InputStream input = Main.class.getResourceAsStream("/invoice-template-pl.properties")) {
-            // load a properties file
-            prop.load(new InputStreamReader(input, StandardCharsets.UTF_8));
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        try (var in = Main.class.getResourceAsStream("/template.html")) {
-            String text = new BufferedReader(
-                    new InputStreamReader(in, StandardCharsets.UTF_8)).lines()
-                    .collect(Collectors.joining("\n"));
-
-            Template t = new Template("name", new StringReader(text),
-                    new Configuration(Configuration.VERSION_2_3_31));
-
-            try (StringWriter out = new StringWriter()) {
-                t.process(prop, out);
-                String toString = out.getBuffer().toString();
-                System.out.println(toString);
-                try (OutputStream os = new FileOutputStream("out.pdf")) {
-                    PdfRendererBuilder builder = new PdfRendererBuilder();
-                    builder.useFastMode();
-                    Document doc = html5ParseDocument(toString);
-                    Element secondItem = doc.getElementById("secondItem");
-//                    doc.getChildNodes()
-//                    doc.removeChild(secondItem);
-                    builder.withW3cDocument(doc, "/");
-                    builder.useFont(() -> Main.class.getResourceAsStream("/SourceSans3-Regular.ttf"),"Noto Sans");
-                    builder.toStream(os);
-                    builder.run();
-                }
-
-                out.flush();
-            }
+//package com.gmail.pzalejko.invoicegenerator.application;
 //
-//            Configuration cfg = new Configuration();
-//            Template template = cfg.getTemplate("/template.html");
-
-        }
-    }
-
-    private static Document html5ParseDocument(String inputHTML) {
-        var doc = Jsoup.parse(inputHTML);
-        return new W3CDom().fromJsoup(doc);
-    }
-}
+//import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+//import freemarker.template.Configuration;
+//import freemarker.template.Template;
+//import freemarker.template.TemplateException;
+//import org.jsoup.Jsoup;
+//import org.jsoup.helper.W3CDom;
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Element;
+//
+//import java.io.*;
+//import java.nio.charset.StandardCharsets;
+//import java.util.Properties;
+//import java.util.stream.Collectors;
+//
+//public class Main {
+//
+//    public static void main(String[] args) throws IOException, TemplateException {
+//        Properties prop = new Properties();
+//        try (InputStream input = Main.class.getResourceAsStream("/invoice-template-pl.properties")) {
+//            // load a properties file
+//            prop.load(new InputStreamReader(input, StandardCharsets.UTF_8));
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//        try (var in = Main.class.getResourceAsStream("/template.html")) {
+//            String text = new BufferedReader(
+//                    new InputStreamReader(in, StandardCharsets.UTF_8)).lines()
+//                    .collect(Collectors.joining("\n"));
+//
+//            Template t = new Template("name", new StringReader(text),
+//                    new Configuration(Configuration.VERSION_2_3_31));
+//
+//            try (StringWriter out = new StringWriter()) {
+//                t.process(prop, out);
+//                String toString = out.getBuffer().toString();
+//                System.out.println(toString);
+//                try (OutputStream os = new FileOutputStream("out.pdf")) {
+//                    PdfRendererBuilder builder = new PdfRendererBuilder();
+//                    builder.useFastMode();
+//                    Document doc = html5ParseDocument(toString);
+//                    Element secondItem = doc.getElementById("secondItem");
+////                    doc.getChildNodes()
+////                    doc.removeChild(secondItem);
+//                    builder.withW3cDocument(doc, "/");
+//                    builder.useFont(() -> Main.class.getResourceAsStream("/SourceSans3-Regular.ttf"),"Noto Sans");
+//                    builder.toStream(os);
+//                    builder.run();
+//                }
+//
+//                out.flush();
+//            }
+////
+////            Configuration cfg = new Configuration();
+////            Template template = cfg.getTemplate("/template.html");
+//
+//        }
+//    }
+//
+//    private static Document html5ParseDocument(String inputHTML) {
+//        var doc = Jsoup.parse(inputHTML);
+//        return new W3CDom().fromJsoup(doc);
+//    }
+//}
--- invoice-generator/src/test/java/com/gmail/pzalejko/invoicegenerator/InvoiceGeneratorLambdaHandlerTest.java
+++ invoice-generator/src/test/java/com/gmail/pzalejko/invoicegenerator/InvoiceGeneratorLambdaHandlerTest.java
@@ -1,35 +1,35 @@
-//package com.gmail.pzalejko.invoicegenerator;
-//
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.gmail.pzalejko.invoicegenerator.application.InvoiceRequestOutput;
-//import io.quarkus.amazon.lambda.test.LambdaClient;
-//import io.quarkus.test.junit.QuarkusTest;
-//import org.apache.commons.io.IOUtils;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
-//
-//@QuarkusTest
-//public class InvoiceGeneratorLambdaHandlerTest {
-//
-//    private JsonNode samplePayload;
-//
-//    @BeforeEach
-//    void setUp() throws IOException {
-//        try (var in = InvoiceGeneratorLambdaHandlerTest.class.getResourceAsStream("/payload.json")) {
-//            var asString = IOUtils.toString(in, StandardCharsets.UTF_8);
-//            var mapper = new ObjectMapper();
-//            samplePayload = mapper.readTree(asString);
-//        }
-//    }
-//
-//    @Test
-//    public void testSimpleLambdaSuccess() {
-//        InvoiceRequestOutput out = LambdaClient.invoke(InvoiceRequestOutput.class, samplePayload);
-//        Assertions.assertNotNull(out);
-//    }
-//}
+package com.gmail.pzalejko.invoicegenerator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gmail.pzalejko.invoicegenerator.application.InvoiceRequestOutput;
+import io.quarkus.amazon.lambda.test.LambdaClient;
+import io.quarkus.test.junit.QuarkusTest;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@QuarkusTest
+public class InvoiceGeneratorLambdaHandlerTest {
+
+    private JsonNode samplePayload;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        try (var in = InvoiceGeneratorLambdaHandlerTest.class.getResourceAsStream("/payload.json")) {
+            var asString = IOUtils.toString(in, StandardCharsets.UTF_8);
+            var mapper = new ObjectMapper();
+            samplePayload = mapper.readTree(asString);
+        }
+    }
+
+    @Test
+    public void testSimpleLambdaSuccess() {
+        InvoiceRequestOutput out = LambdaClient.invoke(InvoiceRequestOutput.class, samplePayload);
+        Assertions.assertNotNull(out);
+    }
+}

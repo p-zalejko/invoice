9ae70849b13ed0b96fba8672eea51ed58029a09d
Pawel Zalejko <p.zalejko@gmail.com>
2021-06-11 00:08:18 +0200

    clean up

--- invoice-generator/src/test/java/com/gmail/pzalejko/invoicegenerator/model/InvoiceInputTest.java
+++ invoice-generator/src/test/java/com/gmail/pzalejko/invoicegenerator/model/InvoiceInputTest.java
@@ -13,8 +13,20 @@ class InvoiceInputTest {
     }
 
     @Test
+    void totalTaxValue_forOne() {
+        InvoiceInput invoiceInput = InvoiceInputTestData.testData(1);
+        assertThat(1d).isEqualTo(invoiceInput.totalTaxValue());
+    }
+
+    @Test
     void totalPriceValue_forMany() {
         InvoiceInput invoiceInput = InvoiceInputTestData.testData(5);
         assertThat(150d).isEqualTo(invoiceInput.totalPriceValue());
+    }
+
+    @Test
+    void totalTaxValue_forMany() {
+        InvoiceInput invoiceInput = InvoiceInputTestData.testData(5);
+        assertThat(15d).isEqualTo(invoiceInput.totalTaxValue());
     }
 }

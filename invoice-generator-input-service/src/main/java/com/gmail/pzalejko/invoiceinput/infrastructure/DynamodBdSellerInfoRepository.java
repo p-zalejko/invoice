b476912066403f54554072893324add114a87f94
Pawel Zalejko <p.zalejko@gmail.com>
2021-06-13 13:58:11 +0200

    setup of a new lambda

--- invoice-generator-input-service/src/main/java/com/gmail/pzalejko/invoiceinput/infrastructure/DynamodBdSellerInfoRepository.java
+++ invoice-generator-input-service/src/main/java/com/gmail/pzalejko/invoiceinput/infrastructure/DynamodBdSellerInfoRepository.java
@@ -3,7 +3,13 @@ package com.gmail.pzalejko.invoiceinput.infrastructure;
 import com.gmail.pzalejko.invoiceinput.model.SellerInfoRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @ApplicationScoped
 public class DynamodBdSellerInfoRepository implements SellerInfoRepository {
+
+    @Inject
+    private DynamoDbClient client;
 }

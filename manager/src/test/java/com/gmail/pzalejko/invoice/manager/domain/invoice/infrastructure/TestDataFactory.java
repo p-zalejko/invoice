a3c623a43a1a2eaf8e24cd6eb2ada06f22470f22
Pawel Zalejko <p.zalejko@gmail.com>
2022-12-04 22:32:12 +0100

    update

--- manager/src/test/java/com/gmail/pzalejko/invoice/manager/domain/invoice/infrastructure/TestDataFactory.java
+++ manager/src/test/java/com/gmail/pzalejko/invoice/manager/domain/invoice/infrastructure/TestDataFactory.java
@@ -10,14 +10,27 @@ public class TestDataFactory {
     public static Company newCompany() {
         var faker = new Faker();
         var fakeAddress = faker.address();
-        var address = new Address(fakeAddress.streetAddress(), fakeAddress.buildingNumber(), fakeAddress.zipCode(), fakeAddress.city(), fakeAddress.country());
+        var address = new Address(
+                normalize(fakeAddress.streetAddress(), 25),
+                normalize(fakeAddress.buildingNumber(), 10),
+                normalize(fakeAddress.zipCode(), 10),
+                normalize(fakeAddress.city(), 25),
+                normalize(fakeAddress.country(), 25)
+        );
 
         return new Company(
                 null,
-                new Name(faker.company().name()),
+                new Name(normalize(faker.company().name(), 25)),
                 address,
                 new TaxId("001"),
                 new BankAccountNumber("000")
         );
+    }
+
+    private String normalize(String value, int maxLength) {
+        if (value.length() > maxLength) {
+            return value.substring(0, maxLength - 1);
+        }
+        return value;
     }
 }

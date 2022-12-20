d3797f7298b4cc7998f1e201f982106468d4fe0d
Pawel Zalejko <p.zalejko@gmail.com>
2022-12-20 21:21:20 +0100

    pdf generator init

---
+++ pdf-generator-service/src/main/java/com/gmail/pzalejko/invoice/pdfgenerator/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.gmail.pzalejko.invoice.pdfgenerator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+class SecurityConfiguration {
+
+    @Value("${system.user.username:demo}")
+    private String username;
+    @Value("${system.user.password:demo}")
+    private String password;
+    @Value("${system.user.roles:USER}")
+    private String[] roles;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf().disable()
+                .authorizeHttpRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User
+                .withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles(roles)
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ pdf-generator-service/src/test/java/com/gmail/pzalejko/invoice/pdfgenerator/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gmail.pzalejko.invoice.pdfgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
